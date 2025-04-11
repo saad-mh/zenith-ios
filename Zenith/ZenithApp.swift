7b2b56ef7a5d6306b0de8bc83c8b18cffedfd872
Saad <saad.ans2018@gmail.com>
2025-04-11 17:02:09 +0530

    Initial Commit

---
+++ Zenith/ZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenithApp.swift
+//  Zenith
+//
+//  Created by Mohammed Saad on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
